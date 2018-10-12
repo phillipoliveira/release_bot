9298e612d03ef807499b900f455cf553d69c0f77
Phillip Oliveira <phillip.j.oliveira@gmail.com>
2018-10-12 15:08:21 -0400

    damn you mike

--- src/app.py
+++ src/app.py
@@ -41,7 +41,7 @@ def post_install():
 def events():
     event_data = json.loads(request.data.decode('utf-8'))
     # Echo the URL verification challenge code back to Slack
-    pattern = re.compile("(rel_.* release notes|deploying rel_.* staging)")
+    pattern = re.compile("(rel_.* release notes|deploying rel_.* staging|deployed rel.* staging)")
     Logging.add_entry(event_data)
     if "challenge" in event_data:
         return make_response(
