6c850dc40f112aa80114fe07b3bc0919c7c5cc92
Phillip Oliveira <phillip.j.oliveira@gmail.com>
2018-10-28 23:46:09 -0400

    bug

--- src/app.py
+++ src/app.py
@@ -121,6 +121,7 @@ def authenticate_request(passed_request):
     ts = passed_request.headers['X-Slack-Request-Timestamp']
     if abs(int(ts) - time.time()) > 360:
         return False
+    print(passed_request.args)
     request_body = urlencode(json.loads(passed_request.data.decode('utf-8')), doseq=True)
     print(request_body)
     slack_signing_secret = SlackCommands.get_app_credentials()['signing_secret'].encode('utf-8')
